9b483042e5e23961b54577d5b7f69b4966e3d278
Tatjana Bezsazna <Tatjana Bezsazna>
2017-03-26 13:18:22 +0300

    Example page styling

--- app/components/Example.jsx
+++ app/components/Example.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 /*var Example = React.createClass({
 	render: function() {
@@ -13,8 +14,16 @@ var React = require('react');
 var Example = (props) => {
 	return (
 		<div>
-			<h3>Examples</h3>
-			<p>Welcome to examples page!</p>
+			<h1 className='text-center'>Examples</h1>
+			<p>Here are a few example locations to try out</p>
+			<ol>
+				<li>
+					<Link to='/?location=Vilnius'>Vilnius, LT</Link>
+				</li>
+				<li>
+					<Link to='/?location=Toronto'>Toronto, CA</Link>
+				</li>
+			</ol>
 		</div>
 		
 	)
